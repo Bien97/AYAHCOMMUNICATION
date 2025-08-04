3ffb5bd62c28d9ebbe1e2372b2f017b342eb9615
N'GUILABOU <nguilaboubienvenu97@gmail.com>
2025-08-04 11:11:10 +0000

    Update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,12 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-<<<<<<< HEAD
 return new class extends Migration {
-=======
-return new class extends Migration
-{
->>>>>>> 757e0377e7441c5f48492a3f6de9194ff7ea16ee
     /**
      * Run the migrations.
      */
@@ -17,30 +12,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-<<<<<<< HEAD
             $table->string('firstname');
             $table->string('lastname');
             $table->string('pseudo')->unique();
             $table->string('email')->nullable();
-=======
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
->>>>>>> 757e0377e7441c5f48492a3f6de9194ff7ea16ee
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
 
-<<<<<<< HEAD
-=======
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
->>>>>>> 757e0377e7441c5f48492a3f6de9194ff7ea16ee
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
             $table->foreignId('user_id')->nullable()->index();
@@ -57,13 +37,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-<<<<<<< HEAD
         Schema::dropIfExists('sessions');
     }
 };
-=======
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
-    }
-};
->>>>>>> 757e0377e7441c5f48492a3f6de9194ff7ea16ee
